e700f681cfdfcdae98598909077f8c55b4b5aba0
Alessandro Venturini <ales.ventus21@gmail.com>
2025-06-05 18:47:44 +0200

    cleaner main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,37 +2,62 @@ import fsm.RoombaFSM
 import fsm.FSM.*
 import domain.Roomba.*
 
-def parseRooms(s: String): Set[String] =
-  s.split(",").map(_.trim()).toSet
-object MainFSM extends App:
-
-  val roomba = for
-    name <- Right(sys.env.get("NAME").getOrElse("Roomba"))
+def parseBattery: Either[String, Int] =
+  for
     batteryStr <- Right(sys.env.get("BATTERY"))
     battery <- batteryStr match
       case None        => Right(50)
       case Some(value) => value.toIntOption.toRight("Battery should be integer")
+  yield (battery)
+
+def parseBatteryRate: Either[String, Long] =
+  for
     batteryRateMsStr <- Right(sys.env.get("BATTERY_RATE_MS"))
     batteryRateMs <- batteryRateMsStr match
       case None => Right(1000)
       case Some(value) =>
         value.toIntOption.toRight("Battery rate should be integer")
+  yield (batteryRateMs)
+
+def parseChangeRoomRate: Either[String, Long] =
+  for
+    changeRoomRateMsStr <- Right(sys.env.get("CHANGE_ROOM_RATE_MS"))
+    changeRoomRateMs <- changeRoomRateMsStr match
+      case None => Right(4000)
+      case Some(value) =>
+        value.toIntOption.toRight("Change room rate should be integer")
+  yield (changeRoomRateMs)
+
+def parseMode: Either[String, Mode] =
+  for
     modeStr <- Right(sys.env.get("MODE"))
     mode <- modeStr match
       case Some("Silent")             => Right(Mode.Silent)
       case Some("DeepCleaning")       => Right(Mode.DeepCleaning)
       case Some("Performance") | None => Right(Mode.Performance)
       case Some(other) => Left(s"$other is not a valid value for MODE")
-    changeRoomRateMsStr <- Right(sys.env.get("CHANGE_ROOM_RATE_MS"))
-    changeRoomRateMs <- batteryRateMsStr match
-      case None => Right(4000)
-      case Some(value) =>
-        value.toIntOption.toRight("Change room rate should be integer")
-    roomsStr <- Right(sys.env.get("ROOMS").map(parseRooms(_)))
+  yield (mode)
+
+def parseRooms: Either[String, Set[String]] =
+  for
+    roomsStr <- Right(
+      sys.env.get("ROOMS").map(_.split(",").map(_.trim()).toSet)
+    )
     rooms <- roomsStr match
       case None => Right(Set("Kitchen", "Bedroom", "Livingroom", "Bathroom"))
       case Some(value) if value.size > 1 => Right(value)
       case _ => Left("At least one room should be given")
+  yield (rooms)
+
+object MainFSM extends App:
+
+  val roomba = for
+    name <- Right(sys.env.get("NAME").getOrElse("Roomba"))
+    battery <- parseBattery
+    batteryRateMs <- parseBatteryRate
+    mode <- parseMode
+    changeRoomRateMs <- parseChangeRoomRate
+    rooms <- parseRooms
     initRoom <- Right(sys.env.get("INIT_ROOM").getOrElse(rooms.head))
     chargingRoom <- Right(sys.env.get("CHARGING_ROOM").getOrElse(rooms.last))
     roomba <- Roomba(
