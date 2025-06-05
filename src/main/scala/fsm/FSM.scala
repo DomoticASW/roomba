317c5364bafcbbf6c78fec1cbd706f315d32d5c3
Alessandro Venturini <ales.ventus21@gmail.com>
2025-06-05 11:45:54 +0200

    FSM state monad

--- src/main/scala/fsm/FSM.scala
+++ src/main/scala/fsm/FSM.scala
@@ -5,44 +5,72 @@ import state.State.*
 object FSM:
 
   opaque type FSM[S, D, E] = FSMImpl[S, D, E]
-  private case class FSMImpl[S, D, E](s: S, d: D)(using States[S, D, E])
+  private case class FSMImpl[S, D, E](s: S, d: D, c: Countdowns)
+
+  private type Countdowns = Map[String, Countdown]
+  private case class Countdown(value: Long, resetValue: Long):
+    def reset: Countdown = Countdown(resetValue, resetValue)
+    def decrement(ms: Long): Countdown = Countdown(value - ms, resetValue)
 
   extension [S, D, E](fsm: FSM[S, D, E])
-    def step(ms: Long, e: Option[E])(using States[S, D, E]): FSM[S, D, E] =
-      val states = summon[States[S, D, E]]
+    def step(ms: Long, e: Option[E])(using S: States[S, D, E]): FSM[S, D, E] =
       val step = for
-        newState <- states.onActive(fsm.s, e, ms)
+        _ <- updateCountdowns(ms)
+        prevState <- S.currentState
+        newState <- S.onActive(e, ms)
         _ <-
-          if newState != fsm.s then
+          if newState != prevState then
             for
-              _ <- states.onExit(fsm.s)
-              _ <- states.onEntry(newState)
+              _ <- S.onExit()
+              _ <- State.modify((_: FSM[S, D, E]).copy(s = newState))
+              _ <- S.onEntry()
             yield ()
           else State.same
       yield (newState)
-      val (newD, newState) = step.run(fsm.d)
-      fsm.copy(s = newState, d = newD)
+      step.run(fsm)._1
 
   object FSM:
-    def apply[S, D, E](s: S, d: D)(using States[S, D, E]): FSM[S, D, E] =
-      val (d1, _) = summon[States[S, D, E]].onEntry(s).run(d)
-      FSMImpl(s, d1)
+    def apply[S, D, E](s: S, d: D)(using S: States[S, D, E]): FSM[S, D, E] =
+      val fsm1 = FSMImpl[S, D, E](s, d, Map.empty)
+      S.onEntry().run(fsm1)._1
 
-    def countdownReached[D](name: String): State[D, Boolean] = ???
-    def setCountdown[D](name: String, ms: Long): State[D, Unit] = ???
-    def resetCountdown[D](name: String): State[D, Unit] = ???
-    def resetCountdownIfReached[D](name: String): State[D, Boolean] =
+    def modified[S, D, E](f: D => D): State[FSM[S, D, E], Unit] =
+      State.modify(fsm => fsm.copy(d = f(fsm.d)))
+    def inspect[S, D, E, A](f: D => A): State[FSM[S, D, E], A] =
+      State.inspect(fsm => f(fsm.d))
+    def same[S, D, E]: State[FSM[S, D, E], Unit] = State.same
+
+    def countdownReached[S, D, E](name: String): State[FSM[S, D, E], Boolean] =
+      State.inspect(_.c.get(name).map(_.value <= 0).getOrElse(false))
+    def setCountdown[S, D, E](
+        name: String,
+        ms: Long
+    ): State[FSM[S, D, E], Unit] =
+      State.modify(fsm => fsm.copy(c = fsm.c + (name -> Countdown(ms, ms))))
+    def resetCountdown[S, D, E](name: String): State[FSM[S, D, E], Unit] =
+      State.modify(fsm => fsm.copy(c = fsm.c.updatedWith(name)(_.map(_.reset))))
+    def resetCountdownIfReached[S, D, E](
+        name: String
+    ): State[FSM[S, D, E], Boolean] =
       for
         reached <- countdownReached(name)
         _ <- if reached then resetCountdown(name) else State.same
       yield (reached)
-    def setCountdownIfReached[D](name: String, ms: Long): State[D, Boolean] =
+    def setCountdownIfReached[S, D, E](
+        name: String,
+        ms: Long
+    ): State[FSM[S, D, E], Boolean] =
       for
         reached <- countdownReached(name)
         _ <- if reached then setCountdown(name, ms) else State.same
       yield (reached)
 
+  private def updateCountdowns[S, D, E](ms: Long): State[FSM[S, D, E], Unit] =
+    State.modify(fsm => fsm.copy(c = fsm.c.mapValues(_.decrement(ms)).toMap))
+
   trait States[S, D, E]:
-    def onEntry(s: S): State[D, Unit]
-    def onActive(s: S, e: Option[E], timePassed: Long): State[D, S]
-    def onExit(s: S): State[D, Unit]
+    def currentState: State[FSM[S, D, E], S] = State.inspect(_.s)
+    def onEntry(): State[FSM[S, D, E], Unit] = State.same
+    def onActive(e: Option[E], timePassed: Long): State[FSM[S, D, E], S] =
+      State(fsm => (fsm, fsm.s))
+    def onExit(): State[FSM[S, D, E], Unit] = State.same
--- src/main/scala/fsm/RoombaFSM.scala
+++ src/main/scala/fsm/RoombaFSM.scala
@@ -1,6 +1,6 @@
 package fsm
 
-import state.State.*
+import state.State.State
 
 object RoombaFSM:
   import scala.util.Random
@@ -14,77 +14,76 @@ object RoombaFSM:
 
   import FSM.*
 
+  type RoombaFSM = FSM[RoombaState, Roomba, Event]
+
   val batteryRate = 1000
   val changeRoomRate = 4000
 
-  private def setMode(mode: Mode): State[Roomba, Unit] =
-    State.modify((m: Roomba) => m.update(mode = mode))
-  private def incBattery(): State[Roomba, Unit] =
-    State.modify((m: Roomba) => m.update(battery = m.battery + 1))
-  private def decBattery(): State[Roomba, Unit] =
-    State.modify((m: Roomba) => m.update(battery = m.battery - 1))
-  private def changeRoomRandom(): State[Roomba, Unit] =
-    State.modify((m: Roomba) =>
-      m.update(currentRoom = Random().shuffle(m.rooms).head)
+  private def setMode(mode: Mode): State[RoombaFSM, Unit] =
+    FSM.modified((_: Roomba).update(mode = mode))
+  private def incBattery(): State[RoombaFSM, Unit] =
+    FSM.modified((r: Roomba) => r.update(battery = r.battery + 1))
+  private def decBattery(): State[RoombaFSM, Unit] =
+    FSM.modified((r: Roomba) => r.update(battery = r.battery - 1))
+  private def changeRoomRandom(): State[RoombaFSM, Unit] =
+    FSM.modified((r: Roomba) =>
+      r.update(currentRoom = Random().shuffle(r.rooms).head)
     )
 
   given States[RoombaState, Roomba, Event] with
-    def onEntry(s: RoombaState): State[Roomba, Unit] =
-      s match
-        case Cleaning =>
-          for
-            _ <- FSM.setCountdown("battery", batteryRate)
-            _ <- FSM.setCountdown("changeRoom", changeRoomRate)
-          yield ()
-        case GoingCharging => FSM.setCountdown("changeRoom", changeRoomRate)
-        case Charging      => FSM.setCountdown("battery", batteryRate)
+    override def onEntry(): State[RoombaFSM, Unit] =
+      for
+        s <- currentState
+        _ <- s match
+          case Cleaning =>
+            for
+              _ <- FSM.setCountdown("battery", batteryRate)
+              _ <- FSM.setCountdown("changeRoom", changeRoomRate)
+            yield ()
+          case GoingCharging => FSM.setCountdown("changeRoom", changeRoomRate)
+          case Charging      => FSM.setCountdown("battery", batteryRate)
+      yield ()
 
-    def onExit(s: RoombaState): State[Roomba, Unit] = State.same
-    def onActive(
-        s: RoombaState,
+    override def onActive(
         e: Option[Event],
         timePassed: Long
-    ): State[Roomba, RoombaState] =
-      val handleChangeModeEvent =
-        for _ <- e match
-            case Some(Event.ChangeMode(mode)) => setMode(mode)
-            case _                            => State.same
-        yield ()
-      val handleActivities = s match
-        case Cleaning =>
-          for
-            updateBattery <- FSM.resetCountdownIfReached("battery")
-            changeRoom <- FSM.resetCountdownIfReached("changeRoom")
-            _ <- if updateBattery then decBattery() else State.same
-            _ <- if changeRoom then changeRoomRandom() else State.same
-            battery <- State.inspect((_: Roomba).battery)
-            nextState =
-              if battery <= 10 || e == Some(Event.Stop) then GoingCharging
-              else Cleaning
-          yield (nextState)
-        case Charging =>
-          for
-            updateBattery <- FSM.resetCountdownIfReached("battery")
-            _ <- if updateBattery then incBattery() else State.same
-            nextState =
-              if e == Some(Event.Start) then Cleaning
-              else Charging
-          yield (nextState)
-        case GoingCharging =>
-          for
-            changeRoom <- FSM.resetCountdownIfReached("changeRoom")
-            _ <- if changeRoom then changeRoomRandom() else State.same
-            newRoom <- State.inspect((_: Roomba).currentRoom)
-            chargingStationRoom <- State.inspect(
-              (_: Roomba).chargingStationRoom
-            )
-            nextState =
-              if e == Some(Event.Start) then Cleaning
-              else if newRoom == chargingStationRoom then Charging
-              else GoingCharging
-          yield (nextState)
-
+    ): State[RoombaFSM, RoombaState] =
       for
-        _ <- handleChangeModeEvent
-        s <- handleActivities
-      yield (s)
+        _ <- e match
+          case Some(Event.ChangeMode(mode)) => setMode(mode)
+          case _                            => State.same
+        s <- currentState
+        nextState <- s match
+          case Cleaning =>
+            for
+              updateBattery <- FSM.resetCountdownIfReached("battery")
+              changeRoom <- FSM.resetCountdownIfReached("changeRoom")
+              _ <- if updateBattery then decBattery() else State.same
+              _ <- if changeRoom then changeRoomRandom() else State.same
+              battery <- FSM.inspect((_: Roomba).battery)
+              nextState =
+                if battery <= 10 || e == Some(Event.Stop) then GoingCharging
+                else Cleaning
+            yield (nextState)
+          case Charging =>
+            for
+              updateBattery <- FSM.resetCountdownIfReached("battery")
+              _ <- if updateBattery then incBattery() else State.same
+              nextState =
+                if e == Some(Event.Start) then Cleaning
+                else Charging
+            yield (nextState)
+          case GoingCharging =>
+            for
+              changeRoom <- FSM.resetCountdownIfReached("changeRoom")
+              _ <- if changeRoom then changeRoomRandom() else State.same
+              newRoom <- FSM.inspect((_: Roomba).currentRoom)
+              chargingStationRoom <- FSM.inspect(
+                (_: Roomba).chargingStationRoom
+              )
+              nextState =
+                if e == Some(Event.Start) then Cleaning
+                else if newRoom == chargingStationRoom then Charging
+                else GoingCharging
+            yield (nextState)
+      yield (nextState)
